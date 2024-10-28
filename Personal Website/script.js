f0878780f9ad5082ec36f4cdec03fed4e81b9bf4
GUO-Shanshan <gshan@unc.edu>
2024-10-28 12:38:36 -0400

    update

--- Personal Website/script.js
+++ Personal Website/script.js
@@ -1,6 +1,6 @@
 setTimeout(function() {
-    const greetingElement = document.getElementById('greeting'); //if donot have this define, the font size cannot change
-    greetingElement.innerText = "This is Guo, Shanshan's personal website.";
+    const greetingElement = document.getElementById('greeting');
+    greetingElement.innerHTML = 'This is Guo, Shanshan\'s personal website.';
     greetingElement.style.fontSize = '6em'; 
 }, 1000);
 
@@ -15,4 +15,15 @@ hoverArea.addEventListener('mouseenter', (e) => {
 
 hoverArea.addEventListener('mouseleave', () => {
     tooltip.style.display = 'none'; 
+});
+
+const buttons = document.querySelectorAll('.button');
+const content = document.querySelector('.content');
+
+buttons[0].addEventListener('click', () => {
+    content.innerHTML = '<h1>About ME</h1><p>这是关于我的内容。</p>';
+});
+
+buttons[1].addEventListener('click', () => {
+    content.innerHTML = '<h1>Job Opportunity</h1><p>这是工作机会的内容。</p>';
 });
